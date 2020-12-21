f60f9a84b22e8fd13e44b5eebd8fa0a58ea58546
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-12-21 16:31:36 +0800

    fix: Opts非比填

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: youzhao.zhou
  * @Date: 2020-12-20 09:11:19
  * @Last Modified by: youzhao.zhou
- * @Last Modified time: 2020-12-20 20:09:32
+ * @Last Modified time: 2020-12-21 16:31:05
  * @Description 根据ApiList生成Api Typing
  * 1. 通过命令行获取到apiList路径和生成d.ts的路径
  * 2. 以apiList为入口，使用webpack编译apiList，得到编译后的结果
@@ -131,7 +131,7 @@ function getInterfaceStr(api, key) {
    *${isEmptyParams(api.params) ? "" : " @param opts"}
    */
   ${key}(
-    opts: IMiAPI.IApiOpts<${getParamsInterface(api.params)}>,
+    opts${isEmptyParams(api.params) ? "?" : ""}: IMiAPI.IApiOpts<${getParamsInterface(api.params)}>,
   ): Promise<IMiAPI.IApiSuccess<${getResInterface(api.res)}>>;
 `;
 }
